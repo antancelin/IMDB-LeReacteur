fc0491166cf0eb91b1dc3106f075d2b0717cfcaf
Antoine Ancelin <ancelin.ant@gmail.com>
2024-12-02 17:57:30 +0100

    first commit expo x react-native project

--- App.js
+++ App.js
@@ -1,20 +1,140 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import des composants
+import {
+  View,
+  StyleSheet,
+  StatusBar,
+  Text,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import Constants from "expo-constants";
 
-export default function App() {
+// import des icônes
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+
+// import du style
+import styles from "./styles";
+
+// imoprt des images
+import logo from "./assets/imgs/logo-imdb.png";
+import film from "./assets/imgs/film.jpg";
+import anne from "./assets/imgs/anne.jpg";
+import jessica from "./assets/imgs/jessica.jpg";
+import mackenzie from "./assets/imgs/mackenzie.jpg";
+import matthew from "./assets/imgs/matthew.jpg";
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SafeAreaView style={styles.safeAreaView}>
+        <View style={styles.logoView}>
+          <Image source={logo} style={styles.logo} />
+        </View>
+        <ScrollView>
+          <View style={styles.globalContent}>
+            <View style={styles.filmInfos}>
+              <Text style={styles.title}>Interstellar</Text>
+              <View style={styles.filmData}>
+                <Text style={styles.textData}>2014</Text>
+                <Text style={styles.textData}>PG-13</Text>
+                <Text style={styles.textData}>2h 49min</Text>
+                <Text style={styles.textData}>Adventure, Drame, Sci-Fi</Text>
+              </View>
+              <View style={styles.imgAndResume}>
+                <Image source={film} style={styles.filmImg} />
+                <View style={styles.filmResume}>
+                  <Text style={styles.resume}>
+                    A team of eplorers travel through a wormhole in space in an
+                    attempt to ensure humanity's survival.
+                  </Text>
+                  <TouchableOpacity style={styles.button}>
+                    <Text style={styles.textButton}>+ ADD TO WATCHLIST</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <View style={styles.rates}>
+                <View style={styles.star}>
+                  <FontAwesome name="star" size={24} color="#E6B91E" />
+                  <View style={styles.rateStar}>
+                    <Text style={styles.rate01}>8.6</Text>
+                    <Text style={styles.rate02}>/10</Text>
+                  </View>
+                  <Text style={styles.rateNumber}>1.1M</Text>
+                </View>
+                <View style={styles.emptyStar}>
+                  <FontAwesome name="star-o" size={24} color="#fff" />
+                  <Text style={styles.emptyStarText}>RATE THIS</Text>
+                </View>
+                <View style={styles.score}>
+                  <View style={styles.metascoreRate}>
+                    <Text style={styles.metascoreNumber}>74</Text>
+                  </View>
+                  <Text style={styles.metascoreName}>Metascore</Text>
+                  <Text style={styles.metascoreReviews}>46 critic reviews</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.actorInfos}>
+              <View style={styles.cast}>
+                <View style={styles.castTop}>
+                  <Text style={styles.topTitle}>Top Billed Cast</Text>
+                  <TouchableOpacity style={styles.allButton}>
+                    <Text style={styles.textSeeAllButton}>SEE ALL</Text>
+                  </TouchableOpacity>
+                </View>
+                <ScrollView
+                  horizontal={true}
+                  style={styles.actorsCarroussel}
+                  contentContainerStyle={{ gap: 10 }}
+                >
+                  <View style={styles.actor}>
+                    <Image source={matthew} style={styles.actorPic} />
+                    <Text
+                      style={styles.actorRealName}
+                      numberOfLines={1}
+                      ellipsizeMode="tail"
+                    >
+                      Matthew McConaughey
+                    </Text>
+                    <Text style={styles.characterName}>Cooper</Text>
+                  </View>
+                  <View style={styles.actor}>
+                    <Image source={anne} style={styles.actorPic} />
+                    <Text style={styles.actorRealName}>Anne Hathaway</Text>
+                    <Text style={styles.characterName}>Brand</Text>
+                  </View>
+                  <View style={styles.actor}>
+                    <Image source={jessica} style={styles.actorPic} />
+                    <Text style={styles.actorRealName}>Jessica Chastain</Text>
+                    <Text style={styles.characterName}>Murph</Text>
+                  </View>
+                  <View style={styles.actor}>
+                    <Image source={mackenzie} style={styles.actorPic} />
+                    <Text style={styles.actorRealName}>Mackenzie Foy</Text>
+                    <Text style={styles.characterName}>Murph</Text>
+                  </View>
+                </ScrollView>
+                <View style={styles.bottomView}>
+                  <Text style={styles.post}>Director</Text>
+                  <Text style={styles.name}>Christopher Nolan</Text>
+                </View>
+                <View style={styles.bottomView}>
+                  <Text style={styles.post}>Writers</Text>
+                  <Text style={styles.name}>
+                    Jonathan Nolan (written by) and Christopher Nolan (written
+                    by)
+                  </Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+      <StatusBar style="light" />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ styles.js
@@ -0,0 +1,237 @@
+import { StyleSheet, Platform } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+    marginTop: Platform.OS === "android" ? Constants.statusBarHeight : 0,
+  },
+
+  safeAreaView: {
+    gap: 10,
+  },
+
+  logoView: {
+    backgroundColor: "#393939",
+    width: "100%",
+    height: 60,
+    justifyContent: "center",
+    alignItems: "flex-start",
+  },
+
+  logo: {
+    width: 60,
+    height: 30,
+    marginLeft: 10,
+  },
+
+  globalContent: {
+    gap: 10,
+  },
+
+  filmInfos: {
+    backgroundColor: "#212121",
+    paddingLeft: 10,
+    paddingTop: 10,
+  },
+
+  title: {
+    color: "white",
+    fontSize: 38,
+    fontWeight: "400",
+  },
+
+  filmData: {
+    flexDirection: "row",
+    gap: 10,
+  },
+
+  textData: {
+    color: "#868686",
+  },
+
+  imgAndResume: {
+    marginTop: 20,
+    flexDirection: "row",
+    gap: 15,
+  },
+
+  filmImg: {
+    width: 105,
+    height: 150,
+  },
+
+  filmResume: {
+    gap: 15,
+    width: 245,
+  },
+
+  resume: {
+    color: "white",
+  },
+
+  button: {
+    backgroundColor: "#0277BD",
+    width: 245,
+    height: 35,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+  },
+
+  textButton: {
+    color: "#fff",
+    fontWeight: "600",
+  },
+
+  rates: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    paddingVertical: 5,
+  },
+
+  star: {
+    alignItems: "center",
+    gap: 2,
+    height: 70,
+    width: 80,
+  },
+
+  rateStar: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  rate01: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+
+  rate02: {
+    color: "#fff",
+    fontSize: 12,
+  },
+
+  rateNumber: {
+    color: "#727272",
+    fontSize: 11,
+  },
+
+  emptyStar: {
+    height: 70,
+    width: 80,
+    alignItems: "center",
+    gap: 6,
+  },
+
+  emptyStarText: {
+    color: "#fff",
+    fontWeight: "800",
+  },
+
+  score: {
+    height: 70,
+    width: 90,
+    alignItems: "center",
+    gap: 5,
+  },
+
+  metascoreRate: {
+    backgroundColor: "#62C750",
+    width: 23,
+    height: 23,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  metascoreNumber: {
+    color: "#fff",
+  },
+
+  metascoreName: {
+    color: "#fff",
+    fontWeight: "600",
+  },
+
+  metascoreReviews: {
+    color: "#727272",
+    fontSize: 11,
+  },
+
+  actorInfos: {
+    backgroundColor: "#212121",
+    paddingLeft: 10,
+    paddingBottom: 10,
+  },
+
+  cast: {
+    gap: 10,
+  },
+
+  castTop: {
+    flexDirection: "row",
+    paddingVertical: 10,
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+  },
+
+  topTitle: {
+    color: "#fff",
+    fontSize: 22,
+  },
+
+  allButton: {
+    marginRight: 10,
+    fontSize: 12,
+  },
+  textSeeAllButton: {
+    color: "#0277BD",
+    fontWeight: "700",
+  },
+
+  actorsCarroussel: {
+    flexDirection: "row",
+    gap: 5,
+  },
+
+  actor: {
+    backgroundColor: "#2A2A2A",
+    width: 145,
+    height: 275,
+    gap: 10,
+  },
+
+  actorPic: {
+    width: 145,
+    height: 215,
+  },
+
+  actorRealName: {
+    color: "#fff",
+    marginLeft: 10,
+  },
+
+  characterName: {
+    color: "#565656",
+    marginLeft: 10,
+  },
+
+  bottomView: {
+    gap: 3,
+  },
+
+  post: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+
+  name: {
+    color: "#565656",
+    fontSize: 14,
+  },
+});
+
+export default styles;
